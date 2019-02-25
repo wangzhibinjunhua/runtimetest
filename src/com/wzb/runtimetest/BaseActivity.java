830cbb6384dea4bc0d1ef642c165c7fffae959fe
wzb <wangzhibin_x@foxmail.com>
2019-02-25 16:30:17 +0800

    init

---
+++ src/com/wzb/runtimetest/BaseActivity.java
@@ -0,0 +1,65 @@
+package com.wzb.runtimetest;
+
+import java.lang.ref.WeakReference;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * @author wzb<wangzhibin_x@qq.com>
+ * @date Feb 25, 2019 2:22:08 PM	
+ */
+
+public class BaseActivity extends Activity{
+
+	
+	public static BaseActivity instance;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		WApplication.activityList.add(this);
+		instance = this;
+	}
+	
+	public static class StaticHandler<T> extends Handler {
+		public WeakReference<T> mWeakReference;
+
+		public StaticHandler(T t) {
+			mWeakReference = new WeakReference<T>(t);
+		}
+	}
+	
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+	}
+	
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
+	
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
+	
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		super.onStop();
+	}
+	
+	
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
+}
---
+++ src/com/wzb/runtimetest/MainActivity.java
@@ -0,0 +1,18 @@
+package com.wzb.runtimetest;
+
+import android.os.Bundle;
+
+/**
+ * @author wzb<wangzhibin_x@qq.com>
+ * @date Feb 25, 2019 2:20:52 PM	
+ */
+public class MainActivity extends BaseActivity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+}
